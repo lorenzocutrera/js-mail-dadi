a56077fd35f3aa8d49a422ac6b9e7595e6b643bb
Lorenzo Cutrera <lorenzoq3ra@gmail.com>
2023-08-29 16:14:32 +0200

    versione realizzata con tutor a seguito di richiesta supporto

---
+++ js/main.js
@@ -0,0 +1,51 @@
+/*
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+Mail
+Chiedi all'utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+Bonus
+Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.
+Consigli del giorno:
+scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+si ma noi cosa vogliamo fare?
+torniamo a scrivere in italiano
+proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+*/
+
+// Generare un numero random per il giocatore
+let numeroRandomComputer = 5; //cercare come generare un numero random da 1 a 6 in javascirp
+//genera un numero random per il computer
+let numeroRandomGiocatore = 1;
+// stabiliamo il vincitore: 
+
+// 1 < 2
+
+// 7 < 1
+
+// paolo < marco
+
+// se numeroRandomComputer < numeroRandomGiocatore
+//il giocatore vince
+//altrimenti se il computer e > del giocatore
+// vince il computer
+//altrimenti pareggio
+
+if(numeroRandomComputer > numeroRandomGiocatore){
+    //stampare il vincitore (computer9)
+}else if(numeroRandomComputer < numeroRandomGiocatore){
+    //stampare il  vincitore (giocatore)
+}else{
+    //stampare il pareggio
+}
+
+/*
+
+    if (condizione o valore boleano){ //espressioni boolean
+        //fai queste istruzioni se vera la condizione = fai se vero
+    }else{
+        //fai queste istruzioni se è falsa la condizione = fai se falso
+    }
+
+*/
